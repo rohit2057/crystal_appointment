7c7ce721ee307a7da5a492f3073a0f2639db8481
rohit2057 <roheetraut2057@gmail.com>
2022-04-30 22:32:59 +0545

    updated

--- app/Http/Controllers/OfficerController.php
+++ app/Http/Controllers/OfficerController.php
@@ -67,38 +67,5 @@ function getOfficerDetail(Request $request)
     return $request->input();
 }
 
-// function officerUpdate(Request $req)
-// {
-//    $req->validate([
-//         'new_first_name' => 'required',
-//         'new_last_name' => 'required',
-//         'new_post' => 'required',
-//         'new_work_start_time' => 'required',
-//         'new_work_end_time' => 'required',
-//     ]);
-    
-//     $id = $req->officer_id;
-//     $obj = Officer::findOrFail($id);
-
-//     $obj->first_name = $req->new_first_name;
-//     $obj->last_name = $req->new_last_name;
-//     $obj->post = $req->new_post;
-//     $obj->work_start_time = $req->new_work_start_time;
-//     $obj->work_end_time = $req->new_work_end_time;
-//     $obj->update();
-
-//     workingdays::where('officer_id', $id)->delete();
-
-//     foreach($req->newdays as $key=>$name)
-//     {
-//         $obj1 = new Workingdays();
-//         $obj1->officer_id = $id;
-//         $obj1->days_of_week = $name;
-//         $obj1->save();
-//     }
-
-//     return redirect()->back()->with('success','You have successfully Updated an Officer');
-// }
-
 
 }
